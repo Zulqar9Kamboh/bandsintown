ebd914a3b6d9af6a89cfe3378ed78cc8a44c9dba
Zee <zulqar9kamboh@gmail.com>
2020-02-21 18:09:17 +0500

    implement the upcomming events of current user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,63 @@
-"use strict";
+'use strict';
 
 $(function () {
+
     //This will show the Artis based on search
     $('#searchArtist').click(function () {
         var artInput = "https://rest.bandsintown.com/artists/" + $('#searchArtistField').val();
-        $.ajax({
-            method: 'GET',
-            url: artInput,
-            data: {
-                app_id: '123'
-            },
-            dataType: 'json',
-            beforeSend: function (xhr) {
-                //wait for the query results
-                $('#searchArtistField').prop('disabled', true);
-            },
-            success: function (result, status, xhr) {
-                // this will execute if correct and fill the data to artist card
-                if (result != null && result != "" && result.error != "Not Found") {
-                    $('#searchArtistField').prop('disabled', false);
-                    $('#artistInfo').show();
-                    $('#noArtistFound').hide();
-                    $('#artistThumb').attr("src", result.thumb_url);
-                    $('#artistName').text(result.name);
-                    $('#artistFB').attr("href", result.facebook_page_url);
-                    if (result.upcoming_event_count > 0) {
-                        $('#upEvents').html('<button class="btn btn-primary mt-1" onclick="getUpcomingEvents()">' + result.upcoming_event_count + ' Upcoming Events</button>');
+        if ($('#searchArtistField').val().length > 0) {
+            $.ajax({
+                method: 'GET',
+                url: artInput,
+                data: {
+                    app_id: '123'
+                },
+                dataType: 'json',
+                beforeSend: function (xhr) {
+                    //wait for the query results
+                    $('#searchArtistField').prop('disabled', true);
+                    $('#artistEventsContainer').empty();
+                },
+                success: function (result, status, xhr) {
+                    // this will execute if correct and fill the data to artist card
+                    if (result != null && result != "" && result.error != "Not Found") {
+                        $('#searchArtistField').prop('disabled', false);
+                        $('#artistInfo').show();
+                        $('#noArtistFound').hide();
+                        $('#artistThumb').attr("src", result.thumb_url);
+                        $('#artistName').text(result.name);
+
+                        //this check tells us if the artis have or haven't facebook
+                        if (result.facebook_page_url == "") {
+                            $('#artistFB').attr("href", "");
+                            $('#artistFB').html('<i class="fa fa-unlink mr-1"></i> Not have facebook page');
+                            $('#artistFB').addClass('disabled');
+                        } else {
+                            $('#artistFB').removeClass('disabled');
+                            $('#artistFB').attr("href", result.facebook_page_url);
+                            $('#artistFB').html('<i class="fab fa-facebook-square mr-1"></i>Find on Facebook');
+
+                        }
+                        //this check tells us is the artis have upcoming events or not
+                        if (result.upcoming_event_count > 0) {
+                            $('#upEvents').html('<button class="btn btn-primary mt-1" onclick="getUpcomingEvents()">' + result.upcoming_event_count + ' Upcoming Events</button>');
+                        } else {
+                            $('#upEvents').html('<b>No Upcoming Events</b>');
+                        }
                     } else {
-                        $('#upEvents').html('<b>No Upcoming Events</b>');
+                        $('#artistInfo').hide();
+                        $('#searchArtistField').prop('disabled', false);
+                        $('#noArtistFound').show().text('No artist found based on your search');
                     }
-                } else {
-                    $('#artistInfo').hide();
-                    $('#searchArtistField').prop('disabled', false);
-                    $('#noArtistFound').show().text('No artist found based on your search');
+                },
+                error: function (xhr, status, error) {
+                    alert("Error Status: " + xhr.status + ", " + xhr.statuText);
+                },
+                complete: function (xhr, status) {
+
                 }
-            },
-            error: function (xhr, status, error) {
-                alert("Status: " + status + ", Error:" + error);
-            },
-            complete: function (xhr, status) {
-                $('#searchArtistField').prop('disabled', false);
-            }
-        });
+            });
+        }
     });
 
     //trigger artist search through enter key
@@ -50,7 +66,17 @@ $(function () {
             $('#searchArtist').click();
         }
     });
-});
+
+    //Back to Search
+    $('#backToSearch').on('click', function () {
+        $('header').removeClass('only-header');
+        $('#artistEventsContainer').empty();
+        $('#artistInfo button').show();
+        $(this).hide();
+    });
+
+})
+
 
 //Upcoming events list
 function getUpcomingEvents() {
@@ -60,16 +86,25 @@ function getUpcomingEvents() {
         url: artName,
         dataType: 'json',
         beforeSend: function (xhr) {
+            $('#artistInfo').addClass('disabled');
 
         },
         success: function (result, status, xhr) {
+            $('#artistInfo').removeClass('disabled');
+            $('#artistInfo button').hide();
+            $('header').addClass('only-header');
+            $('#backToSearch').show();
             var artistEventList = '';
 
+            // this adds the total upcoming events strip
+            artistEventList += '<div class="col-12"><h6 class="up-event-counter">' + result[0].artist.upcoming_event_count + ' Upcoming Events</h6></div>';
+
+            // this loop runs for all upcoming events details
             $.each(result, function (key, val) {
-                if (key == 0) {}
-                artistEventList += '<div class="col-xl-6 mb-3">' +
+                if (key == 0) { }
+                artistEventList += '<div class="col-xl-6 mb-4">' +
                     '<div class="artis-details-card">' +
-                    '<h5>Event Details</h5>' +
+                    '<h5>' + ++key + ': Event Detail</h5>' +
                     '<div class="row gutters-5">' +
                     '<div class="col-6 mb-2">' +
                     '<span>Country:</span>' +
@@ -85,7 +120,7 @@ function getUpcomingEvents() {
                     '</div>' +
                     '<div class="col-6 mb-2">' +
                     '<span>Date:</span>' +
-                    '<strong class="d-block">' + val.datetime + '</strong>' +
+                    '<strong class="d-block">' + showDate(val.datetime) + '</strong>' +
                     '</div>' +
                     '</div>' +
                     '</div>' +
@@ -97,10 +132,22 @@ function getUpcomingEvents() {
 
         },
         error: function (xhr, status, error) {
-
+            alert("Error: " + xhr.status + ", " + xhr.statuText);
         },
         complete: function (xhr, status) {
 
         }
     });
+}
+
+
+//Date Format
+function showDate(dat) {
+    var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'AUg', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var dat = new Date(dat);
+    var m = month[dat.getMonth()];
+    var d = dat.getDate();
+    var y = dat.getFullYear();
+
+    return d + ' ' + m + ', ' + y;
 }
