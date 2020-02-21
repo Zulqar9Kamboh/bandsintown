45610647be3783bf4ff96bb047d7e15035492941
zulqar9kamboh <zulqar9kamboh@gmail.com>
2020-02-21 10:08:14 +0500

    artist upcoming event list is implement

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ $(function () {
                     $('#artistName').text(result.name);
                     $('#artistFB').attr("href", result.facebook_page_url);
                     if (result.upcoming_event_count > 0) {
-                        $('#upEvents').html('<button class="btn btn-primary mt-1">' + result.upcoming_event_count + ' Upcoming Events</button>');
+                        $('#upEvents').html('<button class="btn btn-primary mt-1" onclick="getUpcomingEvents()">' + result.upcoming_event_count + ' Upcoming Events</button>');
                     } else {
                         $('#upEvents').html('<b>No Upcoming Events</b>');
                     }
@@ -51,3 +51,56 @@ $(function () {
         }
     });
 });
+
+//Upcoming events list
+function getUpcomingEvents() {
+    var artName = "https://rest.bandsintown.com/artists/" + encodeURIComponent($('#artistName').text()) + "/events?app_id=123&date=upcoming";
+    $.ajax({
+        method: 'GET',
+        url: artName,
+        dataType: 'json',
+        beforeSend: function (xhr) {
+
+        },
+        success: function (result, status, xhr) {
+            var artistEventList = '';
+
+            $.each(result, function (key, val) {
+                if (key == 0) {}
+                artistEventList += '<div class="col-xl-6 mb-3">' +
+                    '<div class="artis-details-card">' +
+                    '<h5>Event Details</h5>' +
+                    '<div class="row gutters-5">' +
+                    '<div class="col-6 mb-2">' +
+                    '<span>Country:</span>' +
+                    '<strong class="d-block">' + val.venue.country + '</strong>' +
+                    '</div>' +
+                    '<div class="col-6 mb-2">' +
+                    '<span>City:</span>' +
+                    '<strong class="d-block">' + val.venue.city + '</strong>' +
+                    '</div>' +
+                    '<div class="col-6 mb-2">' +
+                    '<span>Venue:</span>' +
+                    '<strong class="d-block">' + val.venue.name + '</strong>' +
+                    '</div>' +
+                    '<div class="col-6 mb-2">' +
+                    '<span>Date:</span>' +
+                    '<strong class="d-block">' + val.datetime + '</strong>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
+
+            });
+
+            $('#artistEventsContainer').html(artistEventList);
+
+        },
+        error: function (xhr, status, error) {
+
+        },
+        complete: function (xhr, status) {
+
+        }
+    });
+}
